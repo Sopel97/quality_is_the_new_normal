aed2b2a9b92a7fbdcad76c4df5dceca3098116ec
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2025-01-16 22:01:20 +0100

    Handle furnace

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -9,5 +9,13 @@ do
         end
     end
 
+    local function alter_furnaces(max_quality_level)
+        local crafting_speed_multiplier = 1.0 / (1.0 + CRAFTING_SPEED_INCREASE_PER_QUALITY_LEVEL * max_quality_level)
+        for key, prototype in pairs(data.raw["furnace"]) do
+            prototype.crafting_speed = prototype.crafting_speed * crafting_speed_multiplier
+        end
+    end
+
     alter_assembling_machines(MAX_QUALITY_LEVEL)
+    alter_furnaces(MAX_QUALITY_LEVEL)
 end
