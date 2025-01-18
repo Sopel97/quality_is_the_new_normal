c92370eeb6540d7cd7013e23966647e065e91c13
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2025-01-18 14:53:28 +0100

    Add notes on unhandled types

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -203,4 +203,20 @@ do
     alter_reactors(MAX_QUALITY_LEVEL)
     alter_wagons(MAX_QUALITY_LEVEL)
     alter_roboports(MAX_QUALITY_LEVEL)
+
+    -- Things left as is by design
+    --    - robots - because they would be too bad to be workable at all
+    --    - chests - because they don't really matter anyway
+    --    - military - because biters are annoying as is in SA and it will be even worse with this mod
+    --    - poles - because there's no way to scale them back and are not that important anyway
+    --    - health - doesn't matter
+    --    - equipment grid sizes - because it's just QoL
+    --    - repair packs - because whatever
+    --    - train fuels - because they are in a good state, though this is controversial, may change at some point
+    --    - science value - because making quality science in the base game is not a good idea
+    --    - thrusters - because you need a lot anyway, may change in the future
+    --    - asteroid collectors - because there's no good way to scale them back, may change in the future
+    --    - exoskeletons - because it's QoL
+    --    - quality modules - because they are at a good point and lowering changes would completely derail this mod
+    --    - drills - no good way of scaling them, might consider a constant multiplier if they end up being way too overpowered with the SA mining productivity research
 end
